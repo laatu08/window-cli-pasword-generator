e37dcbb8c941c794d4307bf3622d4db555d7fdcd
laatu08 <partha2002borah@gmail.com>
2025-12-28 23:33:48 +0530

    complusory element added

--- internal/generator/password.go
+++ internal/generator/password.go
@@ -2,53 +2,83 @@ package generator
 
 import (
 	"crypto/rand"
-	"math/big"
 	"errors"
+	"math/big"
 )
 
 // const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
 
 const (
-	uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	lowercase = "abcdefghijklmnopqrstuvwxyz"
-	digitsSet = "0123456789"
+	uppercase  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowercase  = "abcdefghijklmnopqrstuvwxyz"
+	digitsSet  = "0123456789"
 	symbolsSet = "!@#$%^&*()-_=+[]{}<>?/|"
 )
 
-func Generate(length int, upper, lower, digits, symbols bool)(string, error){
+func randomChar(set string) (byte, error) {
+	n, err := rand.Int(rand.Reader, big.NewInt(int64(len(set))))
+	if err != nil {
+		return 0, err
+	}
+	return set[n.Int64()], nil
+}
+
+func Generate(length int, upper, lower, digits, symbols bool) (string, error) {
 	if length <= 0 {
 		return "", errors.New("length must be greater than 0")
 	}
-	
+
+	password := make([]byte, 0, length)
 	charset := ""
 
 	if upper {
+		c, _ := randomChar(uppercase)
+		password = append(password, c)
 		charset += uppercase
 	}
 	if lower {
+		c, _ := randomChar(lowercase)
+		password = append(password, c)
 		charset += lowercase
 	}
 	if digits {
+		c, _ := randomChar(digitsSet)
+		password = append(password, c)
 		charset += digitsSet
 	}
 	if symbols {
+		c, _ := randomChar(symbolsSet)
+		password = append(password, c)
 		charset += symbolsSet
 	}
 
+	// if charset == "" {
+	// 	charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+	// 	// return "", errors.New("select at least one character type")
+	// }
+
 	if charset == "" {
-		charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
-		// return "", errors.New("select at least one character type")
+		return "", errors.New("select at least one character type")
 	}
 
-	password:=make([]byte, length)
+	if length < len(password) {
+		return "", errors.New("length too short for selected rules")
+	}
 
-	for i := 0; i < length; i++ {
-		n,err:=rand.Int(rand.Reader,big.NewInt(int64(len(charset))))
-		if err!=nil{
-			return "",err
+	for len(password) < length {
+		c, err := randomChar(charset)
+		if err != nil {
+			return "", err
 		}
-		password[i]=charset[n.Int64()]
+		password = append(password, c)
 	}
 
-	return string(password),nil
+	// Shuffle password
+	for i := range password {
+		jRand, _ := rand.Int(rand.Reader, big.NewInt(int64(len(password))))
+		j := int(jRand.Int64())
+		password[i], password[j] = password[j], password[i]
+	}
+
+	return string(password), nil
 }
