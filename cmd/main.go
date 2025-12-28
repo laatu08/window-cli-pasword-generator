d9bfd4a4e7fe208228d7bbf2fc6e6c8e82156287
laatu08 <partha2002borah@gmail.com>
2025-12-28 23:41:49 +0530

    count added

--- cmd/main.go
+++ cmd/main.go
@@ -14,14 +14,31 @@ func main() {
 	lower := flag.Bool("lower", false, "Include lowercase letters")
 	digits := flag.Bool("digits", false, "Include digits")
 	symbols := flag.Bool("symbols", false, "Include symbols")
+	count := flag.Int("count", 1, "Number of passwords to generate")
+
 	flag.Parse()
 
-	password, err := generator.Generate(*length, *upper, *lower, *digits, *symbols)
-	if err != nil {
-		fmt.Println("Error:", err)
+	if *count <= 0 {
+		fmt.Println("Error: count must be greater than 0")
 		os.Exit(1)
 	}
 
-	fmt.Println("Generated Password:")
-	fmt.Println(password)
+	fmt.Println("Generated Passwords:")
+
+	for i := 1; i <= *count; i++ {
+		password, err := generator.Generate(
+			*length,
+			*upper,
+			*lower,
+			*digits,
+			*symbols,
+		)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%d) %s\n", i, password)
+	}
+
 }
